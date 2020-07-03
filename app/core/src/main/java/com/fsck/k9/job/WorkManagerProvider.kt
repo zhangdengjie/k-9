239da498f0e8536f65fa1d8b7b64a55a14d38f5b
cketti <cketti@gmail.com>
2020-07-03 02:00:07 +0200

    Set WorkManager's log level to VERBOSE

--- app/core/src/main/java/com/fsck/k9/job/WorkManagerProvider.kt
+++ app/core/src/main/java/com/fsck/k9/job/WorkManagerProvider.kt
@@ -1,6 +1,7 @@
 package com.fsck.k9.job
 
 import android.content.Context
+import android.util.Log
 import androidx.work.Configuration
 import androidx.work.WorkManager
 import androidx.work.WorkerFactory
@@ -8,8 +9,9 @@ import androidx.work.WorkerFactory
 class WorkManagerProvider(private val context: Context, private val workerFactory: WorkerFactory) {
     fun getWorkManager(): WorkManager {
         val configuration = Configuration.Builder()
-                .setWorkerFactory(workerFactory)
-                .build()
+            .setWorkerFactory(workerFactory)
+            .setMinimumLoggingLevel(Log.VERBOSE)
+            .build()
 
         WorkManager.initialize(context, configuration)
 
